27b922ebb90b48fcca4413f0e0f3897ff9876d8a
Ned Schwartz <ned@theinterned.net>
2021-10-20 17:07:49 -0400

    update brew installations

--- script/install-mac.sh
+++ script/install-mac.sh
@@ -18,7 +18,7 @@ then
   echo "Installing Homebrew"
   echo
 
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew bundle
