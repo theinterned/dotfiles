e7821e407e683843b6e9a4345d32d9a1f69605fa
Ned Schwartz <ned@theinterned.net>
2021-10-21 10:35:51 -0400

    sigh

--- script/fish-setup.fish
+++ script/fish-setup.fish
@@ -18,6 +18,8 @@ end
 if [ (echo $SHELL) != (which fish) ]
   echo "Using Fish shell"
   chsh -s (which fish 2>/dev/null)
+else
+  echo "Already using Fish!"
 end
 
 echo
@@ -31,7 +33,7 @@ echo
 set FISH_CONFIG $HOME/.config/fish/config.fish
 touch $FISH_CONFIG
 
-# install fisher and plugins
+install fisher and plugins
 
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
@@ -39,10 +41,20 @@ fisher install jhillyerd/plugin-git # oh-my-zsh git aliases https://github.com/j
 fisher install jethrokuan/z # jump to directory with memory https://github.com/jethrokuan/z
 fisher install jethrokuan/fzf # command-line fuzzy finder https://github.com/jethrokuan/fzf
 
+
+echo
+echo
+echo "##################################################"
+echo "####### Using Starship prompt ########"
+echo "##################################################"
+echo
+echo
+
 if [ (grep -c starship $FISH_CONFIG) = "0" ]
-  echo "Using starship prompt"
-  echo >> $FISH_CONFIG
+  echo "Using Starship prompt"
   echo "starship init fish | source" >> $FISH_CONFIG
+else
+  echo "Already using Starship"
 end
 
 source $FISH_CONFIG
