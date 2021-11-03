6d27df44e6cf4faa51648c5b3df18448d256376a
Ned Schwartz <ned@theinterned.net>
2021-11-03 10:42:00 -0400

    adds a shortcut to dotfies

---
+++ .config/fish/functions/dotfiles.fish
@@ -0,0 +1,3 @@
+function dotfiles --wraps='cd ~/Documents/src/theinterned/dotfiles' --description 'alias dotfiles cd ~/Documents/src/theinterned/dotfiles'
+  cd ~/Documents/src/theinterned/dotfiles $argv; 
+end
