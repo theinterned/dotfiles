ff06bfe0eabd991e944937a1f8209c7c30828c29
Ned Schwartz <ned@theinterned.net>
2021-11-03 10:42:45 -0400

    Update dotfiles.fish

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,3 +1,3 @@
 function dotfiles --wraps='cd ~/Documents/src/theinterned/dotfiles' --description 'alias dotfiles cd ~/Documents/src/theinterned/dotfiles'
-  cd ~/Documents/src/theinterned/dotfiles $argv; 
+  cd ~/Documents/src/theinterned/dotfiles; 
 end
