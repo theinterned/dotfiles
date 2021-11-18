2eb0b9c8829b0a702db21848b475b5a99512036b
Ned Schwartz <ned@theinterned.net>
2021-11-18 14:57:17 -0500

    moves some fish funcs to abbrs

--- .config/fish/functions/dotfiles.fish
+++
@@ -1,3 +0,0 @@
-function dotfiles --wraps='cd ~/Documents/src/theinterned/dotfiles' --description 'alias dotfiles cd ~/Documents/src/theinterned/dotfiles'
-  cd ~/Documents/src/theinterned/dotfiles; 
-end
