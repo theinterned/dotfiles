6505c3e733ec1d8a8670b87f9e20b8dcc0df9ceb
Ned Schwartz <ned@theinterned.net>
2021-11-18 15:00:00 -0500

    sets abbreviatoins in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,16 @@
 starship init fish | source
 
 set -xg EDITOR "code --wait"
+
+# Rails stuff
+abbr -a -U ss script/server
+abbr -a -U br bin/rails
+abbr -a -U brt bin/rails test
+abbr -a -U bn bin/npm
+abbr -a -U bnt bin/npm test
+
+# Muscle memory git
+abbr -a -U gpf git push --force-with-lease
+
+# shortcut to this project
+abbr -a -U dotfiles cd ~/.dotfiles/
