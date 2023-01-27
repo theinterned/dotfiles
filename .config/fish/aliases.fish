e70b77d1d07858ef3b39d26ea12c977c467be5b5
Ned Schwartz <ned@theinterned.net>
2023-01-27 10:24:09 -0500

    consistant ordering of aliases

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -6,10 +6,31 @@ abbr -aU down 'cd ~/Downloads'
 abbr -aU desk 'cd ~/Desktop'
 abbr -aU src 'cd ~/Documents/src'
 
+# shortcut to this project
+abbr -a -U dotfiles cd ~/.dotfiles
+abbr -a -U config ~/.dotfiles/script/setup.fish
+
 # alloy
 abbr -a -U alloy cd /workspaces/github/vendor/alloy
 abbr -a -U anrd npm run dev --prefix /workspaces/github/vendor/alloy
 abbr -a -U ani npm install --prefix /workspaces/github/vendor/alloy
+
+# npm stuff
+abbr -a -U n npm
+abbr -a -U ni npm install
+abbr -a -U nr npm run
+abbr -a -U nrd npm run dev
+abbr -a -U ns npm start
+abbr -a -U nt npm test
+abbr -a -U ntw npm test:watch
+
+# Rails npm stuff
+abbr -a -U bn bin/npm
+abbr -a -U bni bin/npm install
+abbr -a -U bnr bin/npm run
+abbr -a -U bns bin/npm start
+abbr -a -U bnt bin/npm test
+abbr -a -U bntw bin/npm run test:watch
 
 # Rails stuff
 abbr -a -U ss script/server
@@ -22,14 +43,6 @@ abbr -a -U brt bin/rails test
 abbr -a -U bbe bin/bundle exec
 abbr -a -U bg bin/guard # run tests in watch mode
 
-# Rails npm stuff
-abbr -a -U bn bin/npm
-abbr -a -U bni bin/npm install
-abbr -a -U bnr bin/npm run
-abbr -a -U bns bin/npm start
-abbr -a -U bnt bin/npm test
-abbr -a -U bntw bin/npm run test:watch
-
 # overmind
 abbr -a -U o overmind
 abbr -a -U oc overmind connect
@@ -37,13 +50,8 @@ abbr -a -U os overmind start
 abbr -a -U ost overmind stop
 abbr -a -U ors overmind restart
 
-# npm stuff
-abbr -a -U n npm
-abbr -a -U ni npm install
-abbr -a -U nr npm run
-abbr -a -U nrd npm run dev
-abbr -a -U ns npm start
-abbr -a -U nt npm test
+# Codespaces
+abbr -a -U ncs 'gh cs code -c "$(gh cs create -r github/github --default-permissions)"'
 
 # Muscle memory git
 abbr -a -U gpf git push --force-with-lease
@@ -54,13 +62,10 @@ abbr -a -U gcob git checkout -b theinterned/
 abbr -a -U cww coworking-with 
 abbr -a -U cwws coworking-with --stop
 
-# Codespaces
-abbr -a -U ncs 'gh cs code -c "$(gh cs create -r github/github --default-permissions)"'
-
-# shortcut to this project
-abbr -a -U dotfiles cd ~/.dotfiles
-abbr -a -U config ~/.dotfiles/script/setup.fish
-
 # Other
+abbr -a -U c code
+abbr -a -U edit code
+abbr -a -U e edit
+
 abbr -a -U ffon bin/toggle-feature-flag enable
 abbr -a -U ffoff bin/toggle-feature-flag disable
