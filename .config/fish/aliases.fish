a7af9a0e2d3108bfc35bc930afcc7fa11495a211
Ned Schwartz <ned@theinterned.net>
2023-03-31 11:00:13 -0400

    clean up flags

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -7,70 +7,70 @@ abbr -aU desk 'cd ~/Desktop'
 abbr -aU src 'cd ~/Documents/src'
 
 # shortcut to this project
-abbr -a -U dotfiles cd ~/.dotfiles
-abbr -a -U config ~/.dotfiles/script/setup.fish
+abbr -aU dotfiles cd ~/.dotfiles
+abbr -aU config ~/.dotfiles/script/setup.fish
 
 # alloy
-abbr -a -U alloy cd /workspaces/github/vendor/alloy
-abbr -a -U anrd npm run dev --prefix /workspaces/github/vendor/alloy
-abbr -a -U ani npm install --prefix /workspaces/github/vendor/alloy
+abbr -aU alloy cd /workspaces/github/vendor/alloy
+abbr -aU anrd npm run dev --prefix /workspaces/github/vendor/alloy
+abbr -aU ani npm install --prefix /workspaces/github/vendor/alloy
 
 # npm stuff
-abbr -a -U n npm
-abbr -a -U ni npm install
-abbr -a -U nr npm run
-abbr -a -U nrd npm run dev
-abbr -a -U ns npm start
-abbr -a -U nt npm test
-abbr -a -U ntw npm test:watch
+abbr -aU n npm
+abbr -aU ni npm install
+abbr -aU nr npm run
+abbr -aU nrd npm run dev
+abbr -aU ns npm start
+abbr -aU nt npm test
+abbr -aU ntw npm test:watch
 
 # Rails npm stuff
-abbr -a -U bn bin/npm
-abbr -a -U bni bin/npm install
-abbr -a -U bnr bin/npm run
-abbr -a -U bns bin/npm start
-abbr -a -U bnt bin/npm test
-abbr -a -U bntw bin/npm run test:watch
+abbr -aU bn bin/npm
+abbr -aU bni bin/npm install
+abbr -aU bnr bin/npm run
+abbr -aU bns bin/npm start
+abbr -aU bnt bin/npm test
+abbr -aU bntw bin/npm run test:watch
 
 # Rails stuff
-abbr -a -U ss script/server
-abbr -a -U ssd script/server --debug
-abbr -a -U sss script/dx/server-start
-abbr -a -U sst script/dx/server-stop
-abbr -a -U ssl script/dx/server-logs
-abbr -a -U sb script/bootstrap
-abbr -a -U br bin/rails
-abbr -a -U brt bin/rails test
-abbr -a -U brtc bin/rails test_changes
-abbr -a -U bbe bin/bundle exec
-abbr -a -U bg bin/guard # run tests in watch mode
+abbr -aU ss script/server
+abbr -aU ssd script/server --debug
+abbr -aU sss script/dx/server-start
+abbr -aU sst script/dx/server-stop
+abbr -aU ssl script/dx/server-logs
+abbr -aU sb script/bootstrap
+abbr -aU br bin/rails
+abbr -aU brt bin/rails test
+abbr -aU brtc bin/rails test_changes
+abbr -aU bbe bin/bundle exec
+abbr -aU bg bin/guard # run tests in watch mode
 
 # Stuff
-abbr -a -U bgsf bin/generate-service-files.rb
+abbr -aU bgsf bin/generate-service-files.rb
 
 # overmind
-abbr -a -U o overmind
-abbr -a -U oc overmind connect
-abbr -a -U os overmind start
-abbr -a -U ost overmind stop
-abbr -a -U ors overmind restart
+abbr -aU o overmind
+abbr -aU oc overmind connect
+abbr -aU os overmind start
+abbr -aU ost overmind stop
+abbr -aU ors overmind restart
 
 # Codespaces
-abbr -a -U ncs 'gh cs code -c "$(gh cs create -r github/github --default-permissions)"'
+abbr -aU ncs 'gh cs code -c "$(gh cs create -r github/github --default-permissions)"'
 
 # Muscle memory git
-abbr -a -U gpf git push --force-with-lease
-abbr -a -U gco- git checkout -
-abbr -a -U gco. git checkout .
-abbr -a -U gcob git checkout -b ns/
+abbr -aU gpf git push --force-with-lease
+abbr -aU gco- git checkout -
+abbr -aU gco. git checkout .
+abbr -aU gcob git checkout -b ns/
 
-abbr -a -U cww coworking-with 
-abbr -a -U cwws coworking-with --stop
+abbr -aU cww coworking-with 
+abbr -aU cwws coworking-with --stop
 
 # Other
-abbr -a -U c code
-abbr -a -U edit code
-abbr -a -U e edit
+abbr -aU c code
+abbr -aU edit code
+abbr -aU e edit
 
-abbr -a -U ffon bin/toggle-feature-flag enable
-abbr -a -U ffoff bin/toggle-feature-flag disable
+abbr -aU ffon bin/toggle-feature-flag enable
+abbr -aU ffoff bin/toggle-feature-flag disable
