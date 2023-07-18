93752a7173bf4368cde5b9a9e35180ea0c796927
Ned Schwartz <ned@theinterned.net>
2023-07-18 15:06:46 -0400

    goodbye abbr --universal
    see https://fishshell.com/docs/current/relnotes.html#fish-3-6-0-released-january-7-2023

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,85 +1,85 @@
 #!/usr/bin/env fish
 
 # places
-abbr -aU ~ 'cd ~'
-abbr -aU - 'cd -'
-abbr -aU docs 'cd ~/Documents'
-abbr -aU down 'cd ~/Downloads'
-abbr -aU desk 'cd ~/Desktop'
-abbr -aU src 'cd ~/Documents/src'
+abbr -a ~ 'cd ~'
+abbr -a - 'cd -'
+abbr -a docs 'cd ~/Documents'
+abbr -a down 'cd ~/Downloads'
+abbr -a desk 'cd ~/Desktop'
+abbr -a src 'cd ~/Documents/src'
 
 # shortcut to this project
-abbr -aU dotfiles cd ~/.dotfiles
-abbr -aU config ~/.dotfiles/script/setup.fish
+abbr -a dotfiles cd ~/.dotfiles
+abbr -a config ~/.dotfiles/script/setup.fish
 
 # alloy
-abbr -aU alloy cd /workspaces/github/vendor/alloy
-abbr -aU anrd npm run dev --prefix /workspaces/github/vendor/alloy
-abbr -aU ani npm install --prefix /workspaces/github/vendor/alloy
+abbr -a alloy cd /workspaces/github/vendor/alloy
+abbr -a anrd npm run dev --prefix /workspaces/github/vendor/alloy
+abbr -a ani npm install --prefix /workspaces/github/vendor/alloy
 
 # npm stuff
-abbr -aU n npm
-abbr -aU ni npm install
-abbr -aU nr npm run
-abbr -aU nrd npm run dev
-abbr -aU ns npm start
-abbr -aU nt npm test
-abbr -aU ntw npm test:watch
+abbr -a n npm
+abbr -a ni npm install
+abbr -a nr npm run
+abbr -a nrd npm run dev
+abbr -a ns npm start
+abbr -a nt npm test
+abbr -a ntw npm test:watch
 
 # Rails npm stuff
-abbr -aU bn bin/npm
-abbr -aU bni bin/npm install
-abbr -aU bnr bin/npm run
-abbr -aU bns bin/npm start
-abbr -aU bnt bin/npm test
-abbr -aU bntw bin/npm run test:watch
+abbr -a bn bin/npm
+abbr -a bni bin/npm install
+abbr -a bnr bin/npm run
+abbr -a bns bin/npm start
+abbr -a bnt bin/npm test
+abbr -a bntw bin/npm run test:watch
 
 # Rails stuff
-abbr -aU ss script/server
-abbr -aU ssmd "gh medic && script/server"
-abbr -aU ghm "gh medic"
-abbr -aU ssd script/server --debug
-abbr -aU sss script/dx/server-start
-abbr -aU sst script/dx/server-stop
-abbr -aU ssl script/dx/server-logs
-abbr -aU sb script/bootstrap
-abbr -aU br bin/rails
-abbr -aU brt bin/rails test
-abbr -aU brtc bin/rails test_changes
-abbr -aU bbe bin/bundle exec
-abbr -aU bg bin/guard # run tests in watch mode
+abbr -a ss script/server
+abbr -a ssmd "gh medic && script/server"
+abbr -a ghm "gh medic"
+abbr -a ssd script/server --debug
+abbr -a sss script/dx/server-start
+abbr -a sst script/dx/server-stop
+abbr -a ssl script/dx/server-logs
+abbr -a sb script/bootstrap
+abbr -a br bin/rails
+abbr -a brt bin/rails test
+abbr -a brtc bin/rails test_changes
+abbr -a bbe bin/bundle exec
+abbr -a bg bin/guard # run tests in watch mode
 
 # Stuff
-abbr -aU bgsf bin/generate-service-files.rb
+abbr -a bgsf bin/generate-service-files.rb
 
 # overmind
-abbr -aU o overmind
-abbr -aU oc overmind connect
-abbr -aU os overmind start
-abbr -aU ost overmind stop
-abbr -aU ors overmind restart
+abbr -a o overmind
+abbr -a oc overmind connect
+abbr -a os overmind start
+abbr -a ost overmind stop
+abbr -a ors overmind restart
 
 # Codespaces
-abbr -aU ncs 'gh cs code -c "$(gh cs create -r github/github --default-permissions)"'
+abbr -a ncs 'gh cs code -c "$(gh cs create -r github/github --default-permissions)"'
 
 # Muscle memory git
-abbr -aU gpf git push --force-with-lease
-abbr -aU gco- git checkout -
-abbr -aU gco. git checkout .
-abbr -aU gcob git checkout -b ns/
-abbr -aU gdst git diff --staged
+abbr -a gpf git push --force-with-lease
+abbr -a gco- git checkout -
+abbr -a gco. git checkout .
+abbr -a gcob git checkout -b ns/
+abbr -a gdst git diff --staged
 
-abbr -aU cww coworking-with 
-abbr -aU cwws coworking-with --stop
+abbr -a cww coworking-with 
+abbr -a cwws coworking-with --stop
 
 # Other
-abbr -aU c code
-abbr -aU edit code
-abbr -aU e edit
+abbr -a c code
+abbr -a edit code
+abbr -a e edit
 
-abbr -aU ffon bin/toggle-feature-flag enable
-abbr -aU ffoff bin/toggle-feature-flag disable
+abbr -a ffon bin/toggle-feature-flag enable
+abbr -a ffoff bin/toggle-feature-flag disable
 
 # fix a garbled terminal
 # https://www.oreilly.com/library/view/bash-cookbook/0596526784/ch19s09.html
-abbr -aU sane stty sane
+abbr -a sane stty sane
