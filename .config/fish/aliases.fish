d38506cb01110d87a3a98eb8d13d7eaee3c67350
Ned Schwartz <ned@theinterned.net>
2024-04-10 16:35:37 -0400

    sb alias

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -22,6 +22,7 @@ abbr -a n npm
 abbr -a ni npm install
 abbr -a nr npm run
 abbr -a nrd npm run dev
+abbr -a sb npm run storybook
 abbr -a nsb npm run storybook
 abbr -a ns npm start
 abbr -a nt npm test
