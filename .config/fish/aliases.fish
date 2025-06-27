a4ef1a6481cb473308a4ed984168c1601046a2fe
Ned Schwartz <theinterned@github.com>
2025-06-27 12:34:56 -0400

    ssv > ssw

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -50,7 +50,7 @@ abbr -a bntw bin/npm run test:watch
 
 # Rails stuff
 abbr -a ss SKIP_LOOKBOOK=1 VITE=1 script/server
-abbr -a ssv SKIP_LOOKBOOK=1 script/server
+abbr -a ssw SKIP_LOOKBOOK=1 script/server
 abbr -a ssmd "gh medic && script/server"
 abbr -a ghm "gh medic"
 abbr -a ssd script/server --debug
